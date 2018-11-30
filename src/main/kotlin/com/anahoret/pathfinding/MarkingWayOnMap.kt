7f5f46f30fff2028ec8855d8a9ef8ab7cc65b6d2
aam <aam@anahoret.com>
2018-11-30 10:36:37 +0200

    Refactor.

--- src/main/kotlin/com/anahoret/pathfinding/MarkingWayOnMap.kt
+++ src/main/kotlin/com/anahoret/pathfinding/MarkingWayOnMap.kt
@@ -3,47 +3,29 @@ package com.anahoret.pathfinding
 import java.lang.IllegalArgumentException
 import java.util.*
 
-fun addPath(map: String): String {
-    return Graph(map).getMapWithPath()
-}
+fun addPath(map: String): String = Graph.getMapWithPath(map)
 
-class Node(val row: Int,
-           val col: Int,
-           char: Char) {
+object Graph {
 
-    val isWall = char == 'B'
-    val isPath = !isWall
-    val isStart = char == 'S'
-    val isEnd = char == 'X'
+    private const val STRAIGHT_LENGTH = 2
+    private const val DIAGONAL_LENGTH = 3
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Node) return false
+    fun getMapWithPath(map: String): String {
+        val (nodes, arcs) = readGraph(map)
+        val start = nodes.find { it.isStart } ?: throw IllegalArgumentException("No start point specified")
+        val end = nodes.find { it.isEnd } ?: throw IllegalArgumentException("No end point specified")
 
-        if (row != other.row) return false
-        if (col != other.col) return false
-
-        return true
+        val paths = calcDistances(start, nodes, arcs)
+        return map.lines()
+            .map(String::toCharArray)
+            .let { charGrid ->
+                charGrid[start.row][start.col] = '*'
+                paths.getValue(end).forEach { charGrid[it.row][it.col] = '*' }
+                charGrid.joinToString(separator = "\n") { row -> row.joinToString(separator = "") }
+            }
     }
 
-    override fun hashCode(): Int {
-        var result = row
-        result = 31 * result + col
-        return result
-    }
-
-}
-
-class Arc(val node: Node, val length: Int)
-
-class Graph(private val map: String) {
-    private val graphData = Graph.readGraph(map)
-    private val nodes = graphData.nodes
-    private val arcs = graphData.arcs
-    private val start = nodes.find { it.isStart } ?: throw IllegalArgumentException("No start point specified")
-    private val end = nodes.find { it.isEnd } ?: throw IllegalArgumentException("No end point specified")
-
-    private fun calcDistances(): Map<Node, List<Node>> {
+    private fun calcDistances(start: Node, nodes: Collection<Node>, arcs: Map<Node, List<Arc>>): Map<Node, List<Node>> {
         val paths = nodes.map { it to emptyList<Node>() }.toMap().toMutableMap()
         val distances = nodes.map { it to if (it == start) 0 else Int.MAX_VALUE }.toMap().toMutableMap()
         val visited = mutableSetOf<Node>()
@@ -69,56 +51,58 @@ class Graph(private val map: String) {
         return paths.toMap()
     }
 
-    fun getMapWithPath(): String {
-        val paths = calcDistances()
-        return map.lines()
-            .map { it.toCharArray() }
-            .let { array ->
-                array[start.row][start.col] = '*'
-                paths.getValue(end).forEach { n ->
-                    array[n.row][n.col] = '*'
-                }
-                array
-            }.joinToString(separator = "\n") { it.joinToString(separator = "") }
-    }
-
-    companion object {
-        private const val STRAIGHT_LENGTH = 2
-        private const val DIAGONAL_LENGTH = 3
-
-        private fun List<Node>?.notWallOrNull(idx: Int): Node? = this?.getOrNull(idx)?.takeUnless(Node::isWall)
-
-        private fun readGraph(map: String): GraphData {
-            val nodeGrid = map.lines()
-                .mapIndexed { row, str -> str.mapIndexed { col, char -> Node(row, col, char) } }
-
-            val arcs = mutableMapOf<Node, List<Arc>>().withDefault { emptyList() }
-
-            nodeGrid.map { nodesRow ->
-                nodesRow.map { node ->
-                    val maybeRow = nodeGrid.getOrNull(node.row)
-                    val maybeTopRow = nodeGrid.getOrNull(node.row - 1)
-                    val maybeBottomRow = nodeGrid.getOrNull(node.row + 1)
-                    val nodeArcs = listOfNotNull(
-                        maybeTopRow.notWallOrNull(node.col)?.let { Arc(it, STRAIGHT_LENGTH) },
-                        maybeTopRow.notWallOrNull(node.col - 1)?.let { Arc(it, DIAGONAL_LENGTH) },
-                        maybeTopRow.notWallOrNull(node.col + 1)?.let { Arc(it, DIAGONAL_LENGTH) },
-
-                        maybeBottomRow.notWallOrNull(node.col)?.let { Arc(it, STRAIGHT_LENGTH) },
-                        maybeBottomRow.notWallOrNull(node.col - 1)?.let { Arc(it, DIAGONAL_LENGTH) },
-                        maybeBottomRow.notWallOrNull(node.col + 1)?.let { Arc(it, DIAGONAL_LENGTH) },
-
-                        maybeRow.notWallOrNull(node.col - 1)?.let { Arc(it, STRAIGHT_LENGTH) },
-                        maybeRow.notWallOrNull(node.col + 1)?.let { Arc(it, STRAIGHT_LENGTH) }
-                    )
-                    arcs.put(node, nodeArcs)
-                }
+    private fun readGraph(map: String): Pair<List<Node>, Map<Node, List<Arc>>> {
+        val nodes = map.lines()
+            .mapIndexed { row, str ->
+                str.mapIndexedNotNull { col, char -> if (char == 'B') null else Node(row, col, char) }
             }
+            .flatten()
 
-            return GraphData(nodeGrid.flatten().filter(Node::isPath), arcs)
-        }
+        val arcs = nodes.map { node ->
+            val row = nodes.filter { it.row == node.row }
+            val topRow = nodes.filter { it.row == node.row - 1 }
+            val bottomRow = nodes.filter { it.row == node.row + 1 }
+            val nodeArcs = listOfNotNull(
+                topRow.find { it.col == node.col }?.let { Arc(it, STRAIGHT_LENGTH) },
+                topRow.find { it.col == node.col - 1 }?.let { Arc(it, DIAGONAL_LENGTH) },
+                topRow.find { it.col == node.col + 1 }?.let { Arc(it, DIAGONAL_LENGTH) },
+
+                bottomRow.find { it.col == node.col }?.let { Arc(it, STRAIGHT_LENGTH) },
+                bottomRow.find { it.col == node.col - 1 }?.let { Arc(it, DIAGONAL_LENGTH) },
+                bottomRow.find { it.col == node.col + 1 }?.let { Arc(it, DIAGONAL_LENGTH) },
+
+                row.find { it.col == node.col - 1 }?.let { Arc(it, STRAIGHT_LENGTH) },
+                row.find { it.col == node.col + 1 }?.let { Arc(it, STRAIGHT_LENGTH) }
+            )
+            node to nodeArcs
+        }.toMap()
+
+        return Pair(nodes, arcs)
     }
 
-    private class GraphData(val nodes: List<Node>, val arcs: Map<Node, List<Arc>>)
+    class Node(val row: Int, val col: Int, char: Char) {
+
+        val isStart = char == 'S'
+        val isEnd = char == 'X'
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is Node) return false
+
+            if (row != other.row) return false
+            if (col != other.col) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = row
+            result = 31 * result + col
+            return result
+        }
+
+    }
+
+    class Arc(val node: Node, val length: Int)
 
 }
